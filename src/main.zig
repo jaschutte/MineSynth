e4fb24bd08393618f150682a3543d93c43a40c23
yayaduckd <114066508+yayaduckd@users.noreply.github.com>
2026-03-12 00:39:06 +0100

    route test and small nbt lib change

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,10 @@
 const std = @import("std");
-const pretty = @import("pretty");
 const aiger = @import("aiger.zig");
 const nl = @import("netlist.zig");
-// const partitioning = @import("partitioning.zig");
 const glib = @import("abstract/graph.zig");
 const glibopt = @import("abstract/preprocessor.zig");
 const graphviz = @import("abstract/graphviz.zig");
 const rt = @import("routing.zig");
-
 const nbt = @import("nbt.zig");
 const ms = @import("abstract/structures.zig");
 
@@ -16,7 +13,6 @@ pub fn main() !void {
     const gpa = real_gpa.allocator();
     defer _ = real_gpa.deinit();
 
-    // const content = try std.fs.cwd().readFileAlloc(gpa, "aiger-examples/half-adder.aag", std.math.maxInt(usize));
     const content = try std.fs.cwd().readFileAlloc(gpa, "aiger-examples/serial-adder.aag", std.math.maxInt(usize));
     defer _ = gpa.free(content);
 
@@ -27,29 +23,47 @@ pub fn main() !void {
     defer _ = netlist.deinit();
 
     var graph = glib.GraphConstructors.fromNetlist(gpa, &netlist);
-
     graphviz.GraphVisualizer(glib.GateBody).printDFS(gpa, graph);
     glibopt.PreProcessor(glib.GateBody).preprocess(graph);
     graphviz.GraphVisualizer(glib.GateBody).print(gpa, graph);
     graph.deinit();
 
-    // each layer is a height of 3, so any target y coordinate must be a multiple of 3
-    // forbidden zone should contain all mc blocks inside an AND/NOT block
-    // + input/outputs + blocks adjacent to inputs/outputs
     var forbidden_zone = ms.ForbiddenZone.init(gpa);
     defer forbidden_zone.deinit();
-    var route = try rt.routeToUpdateForbiddenZone(gpa, .{ -20, 0, 0 }, .{ 40, 0, 0 }, &forbidden_zone);
+
+    const test_endpoints = [_][2][3]i32{
+        .{ .{ -20, 0, 0 }, .{ 40, 0, 0 } },
+        .{ .{ 10, 0, -20 }, .{ 10, 0, 20 } },
+        .{ .{ -20, 0, 10 }, .{ 40, 0, 10 } },
+        .{ .{ 30, 0, -30 }, .{ -10, 0, 30 } },
+        .{ .{ -40, 0, -10 }, .{ 20, 0, -10 } },
+        .{ .{ 0, 0, -40 }, .{ 0, 0, 40 } },
+        // .{ .{ 50, 0, -50 }, .{ -20, 0, 20 } },
+        .{ .{ -50, 0, 40 }, .{ 30, 0, 50 } },
+    };
+
+    var master_route = try rt.routeToUpdateForbiddenZone(gpa, test_endpoints[0][0], test_endpoints[0][1], &forbidden_zone);
+    defer master_route.deinit(gpa);
+
     std.log.info(
         "path of length {d} and delay {d} found between {any} and {any}\n",
-        .{ route.length, route.delay, .{ -20, 0, 0 }, .{ 40, 0, 0 } },
+        .{ master_route.length, master_route.delay, test_endpoints[0][0], test_endpoints[0][1] },
     );
-    defer route.deinit(gpa);
-    var route2 = try rt.routeToUpdateForbiddenZone(gpa, .{ 10, 0, -20 }, .{ 10, 0, 20 }, &forbidden_zone);
-    std.log.info(
-        "path of length {d} and delay {d} found between {any} and {any}\n",
-        .{ route2.length, route2.delay, .{ 10, 0, -20 }, .{ 10, 0, 20 } },
-    );
-    defer route2.deinit(gpa);
-    try route.route.appendSlice(gpa, route2.route.items);
-    nbt.abs_block_arr_to_schem(gpa, route.route.items);
+
+    for (test_endpoints[1..]) |endpoints| {
+        const start = endpoints[0];
+        const end = endpoints[1];
+
+        var route = try rt.routeToUpdateForbiddenZone(gpa, start, end, &forbidden_zone);
+        defer route.deinit(gpa);
+
+        std.log.info(
+            "path of length {d} and delay {d} found between {any} and {any}",
+            .{ route.length, route.delay, start, end },
+        );
+
+        try master_route.route.appendSlice(gpa, route.route.items);
+    }
+
+    nbt.abs_block_arr_to_schem(gpa, master_route.route.items);
 }
