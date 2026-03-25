180cbfb3e9d8ab1528a413c38847ffd42f1de1d6
yayaduckd <114066508+yayaduckd@users.noreply.github.com>
2026-03-25 13:23:58 +0100

    merge

--- src/main.zig
+++ src/main.zig
@@ -69,29 +69,5 @@ pub fn main() !void {
 
     defer route.deinit(gpa);
 
-    // var master_route = try rt.routeToUpdateForbiddenZone(gpa, test_endpoints[0][0], test_endpoints[0][1], &forbidden_zone);
-    // defer master_route.deinit(gpa);
-
-    // const master_route = 2;
-    // std.log.info(
-    //     "path of length {d} and delay {d} with violating {any} found between {any} and {any}\n",
-    //     .{ master_route.length, master_route.delay, master_route.violating, test_endpoints[0][0], test_endpoints[0][1] },
-    // );
-
-    // for (test_endpoints[1..]) |endpoints| {
-    //     const start = endpoints[0];
-    //     const end = endpoints[1];
-
-    //     var route = try rt.routeToUpdateForbiddenZone(gpa, start, end, &forbidden_zone);
-    //     defer route.deinit(gpa);
-
-    //     std.log.info(
-    //         "path of length {d} and delay {d} with violating {any} found between {any} and {any}",
-    //         .{ route.length, route.delay, route.violating, start, end },
-    //     );
-
-    //     try master_route.route.appendSlice(gpa, route.route.items);
-    // }
-
     nbt.abs_block_arr_to_schem(gpa, route.route.items);
 }
